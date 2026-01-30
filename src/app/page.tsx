8d1c4b1a254cd2e2ce10b5a212b39fce6243842c
willardjansen <willardjansen@me.com>
2026-01-30 13:16:03 +0100

    Revert "feat: add About modal with newsletter signup"
    This reverts commit ac24cefc6fa2772a17660ca3064d596dfdedc9fc.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect, useCallback } from 'react';
 import { ArticulationGrid } from '@/components/ArticulationGrid';
-import About from '@/components/About';
 import {
   ArticulationSet,
   createDemoArticulationSet,
@@ -385,9 +384,6 @@ export default function Home() {
           </p>
         </div>
       )}
-
-      {/* About Button */}
-      <About />
     </main>
   );
 }
